75a5d32377c29ea38dc66f4c2c9386783815589d
facundo000 <facundo.guzman@mi.unc.edu.ar>
2025-03-03 15:30:50 -0300

    production commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GridWsModule } from './grid-ws/grid-ws.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GridWsModule],
+  imports: [
+    GridWsModule,
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
